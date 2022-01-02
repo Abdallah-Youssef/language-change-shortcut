91c24203f2aed18c6668f8c727712d27acff5304
Abdallah <abdallah.yossf@gmail.com>
2022-01-03 00:16:27 +0200

    Initial code for english to arabic

--- background.js
+++ background.js
@@ -1,19 +1,30 @@
-chrome.contextMenus.create({
-    id: "change-text",
-    title: "Change",
-    contexts: ['editable'] // show only when selection over editable field e.g. <input>
+
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.contextMenus.create({
+        id: "change-text",
+        title: "Change",
+        contexts: ['editable'] // show only when selection over editable field 
+    });
 });
 
 
-const translate = (text) => "عادل شكل"
+const execute = () => {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {});
+    });
+}
 
 
 chrome.contextMenus.onClicked.addListener(function (info, tab) {
     if (info.menuItemId === "change-text") {
-        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, { newText: translate(info.selectionText) });
-        });
+        execute()
+        return
     }
+});
+
+chrome.commands.onCommand.addListener((command) => {
+    if (command === 'run')
+        execute()
 });
 
 
--- script.js
+++ script.js
@@ -1,12 +1,132 @@
-console.log("\n\n\n\n\n\n\n\nInjected\n\n\n\n\n\n\n")
+console.log("\n\n\n\n\n\n\n\nLanguage changer is injected\n\n\n\n\n\n\n")
+
+const isLetter = (str) => str.length === 1 && str.match(/[a-z]/i);
+const reverseCase = (c) =>  c === c.toUpperCase() ? c.toLowerCase() : c.toUpperCase();
+
+
+const translate = (keyStrokes) => {
+    console.log(keyStrokes)
+
+    // All keys here should be lower case
+    let en_to_ar_map = {
+        "`": "ذ",
+        "~": "ّ",
+        "q": "ض",
+        "w": "ص",
+        "e": "ث",
+        "r": "ق",
+        "t": "ف",
+        "y": "غ",
+        "u": "ع",
+        "i": "ه",
+        "I": "÷",
+        "o": "خ",
+        "O": "×",
+        "p": "ح",
+        "P": "؛",
+        "[": "ج",
+        "{": "<",
+        "]": "د",
+        "}": ">",
+        "a": "ش",
+        "A": "ِ",
+        "s": "س",
+        "d": "ي",
+        "f": "ب",
+        "g": "ل",
+        "G": "لأ",
+        "h": "ا",
+        "H": "أ",
+        "j": "ت",
+        "J": "ـ",
+        "k": "ن",
+        "K": "،",
+        "l": "م",
+        "L": "/",
+        ";": "ك",
+        "'": "ط",
+        "z": "ئ",
+        "x": "ء",
+        "c": "ؤ",
+        "v": "ر",
+        "b": "لا",
+        "B": "لآ",
+        "n": "ى",
+        "N": "آ",
+        "m": "ة",
+        ",": "و",
+        "<": ",",
+        ".": "ز",
+        ">": ".",
+        "/": "ظ",
+        "?": "؟",
+        "Enter": "\n",
+    }
+
+    let res = ""
+
+    console.log("Translating: ", keyStrokes)
+
+    keyStrokes.forEach(keyStroke => {
+        if (keyStroke === 'Backspace') 
+            res = res.substring(0, res.length-1)
+        
+        else if (en_to_ar_map[keyStroke]) // If you find a map entry, then use it
+            res += en_to_ar_map[keyStroke]
+        else if (en_to_ar_map[keyStroke.toLowerCase()]) // Try to look for lowercase entry
+            res += en_to_ar_map[keyStroke.toLowerCase()]
+        else res += keyStroke // Use it as is e.g. " ", "+", "_"
+
+        // console.log(keyStroke)
+        // console.log(res)
+    })
+
+    return res
+
+}
+
+
+let keyStrokes = []
+let lastFocusedElement = null
+
+document.addEventListener('keyup', (e) => {
+    // console.log(e.key)
+
+    const ignore = ['Alt', 'Shift', 'Tab', 'Control', 'CapsLock']
+    if (ignore.indexOf(e.key) >= 0)
+        return
+
+    if (e.target !== lastFocusedElement) {
+        keyStrokes = []
+        lastFocusedElement = e.target
+    }
+
+
+    if (isLetter(e.key) && e.getModifierState('CapsLock')){
+        // console.log("letter " + e.key + " capslocked")
+        // console.log("Push " + reverseCase(e.key) + " instead")
+        keyStrokes.push(reverseCase(e.key))
+    }
+    else keyStrokes.push(e.key)
+})
 
 chrome.runtime.onMessage.addListener(
-    function ({newText}, sender, sendResponse) {
+
+    function (req, sender, sendResponse) {
         console.log(sender.tab ?
             "from a content script:" + sender.tab.url :
             "from the extension");
 
+        // console.log(typeof lastFocusedElement)
+        // console.log(Object.keys(lastFocusedElement))
+        // console.log(lastFocusedElement.nodeName)
+        // console.log(lastFocusedElement.value)
+        // console.log(translate(keyStrokes))
 
-        document.activeElement.value = newText
+        lastFocusedElement.value = translate(keyStrokes)
+        lastFocusedElement.innerHTML = translate(keyStrokes)
+        lastFocusedElement.text = translate(keyStrokes)
     }
 );
+
+
